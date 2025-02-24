dae3a256ec902f383c580ade94c10d31f3aea899
Mayur-Shahani <shahanimkwork@gmail.com>
2025-02-24 22:42:57 +0500

    Use All Data Types

---
+++ UseAllDataTypes.java
@@ -0,0 +1,19 @@
+public class UseAllDataTypes {
+    public static void main(String[] args){
+        int age = 20;
+        char ch = 'P';
+        float f = 54.87f;
+        double s = 4.56d;
+        byte b = 4;
+        long l = 5789l;
+        short t = 74;
+
+        System.out.println(age); 
+        System.out.println(ch); 
+        System.out.println(f); 
+        System.out.println(s); 
+        System.out.println(b); 
+        System.out.println(l); 
+        System.out.println(t); 
+    }
+}
