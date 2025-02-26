ad53206624b248bf9108fb175dd232d275eb0f5b
Mayur-Shahani <shahanimkwork@gmail.com>
2025-02-26 22:35:10 +0600

    Check Even And Odd Number

---
+++ Class Program/EvenOdd.java
@@ -0,0 +1,14 @@
+package ClassProgram;
+
+public class EvenOdd {
+    String checkEvenOdd(int x){
+        if(x % 2 != 0)
+        {
+            return "Odd Number";
+        }
+        else
+        {
+            return "Even Number";
+        }
+    }
+}
---
+++ Class Program/Main.java
@@ -0,0 +1,10 @@
+package ClassProgram;
+
+public class Main {
+    public static void main(String[] args){
+        EvenOdd a1 = new EvenOdd();
+        System.out.println(a1.checkEvenOdd(2));
+
+    }
+    
+}
