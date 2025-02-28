b6a538f61be5d0d58faf4f6cb1d70050434a406e
Mayur-Shahani <shahanimkwork@gmail.com>
2025-02-28 10:42:58 +0500

    Use of Airthmetic Operators

---
+++ Airthmetic_Operators.java
@@ -0,0 +1,28 @@
+package ClassProgram;
+
+public class Airthmetic_Operators {
+    public static void main(String[] args){
+        int a = 78;
+        int b = 6;
+        int result;
+       
+        System.out.println("Airthmetic Operators");
+        
+        result = a + b;
+        System.out.println("The addition of a and b is : " +result);
+
+        result = a - b;
+        System.out.println("The subtraction of a and b is : " +result);
+
+        result = a * b;
+        System.out.println("The Multiplication of a and b is : " +result);
+
+        result = a / b;
+        System.out.println("The division of a and b is : " +result);
+
+        result = a % b;
+        System.out.println("The Modulus of a and b is : " +result);
+
+
+    }
+}
