15aec7b4058be331af73da689ac288375b0cbeff
Mayur-Shahani <shahanimkwork@gmail.com>
2025-02-28 10:57:24 +0500

    Assignment Operator

---
+++ Assignment_Operator.java
@@ -0,0 +1,17 @@
+package ClassProgram;
+
+public class Assignment_Operator {
+    public static void main(String[] args) {
+        int a = 8;
+
+        System.out.println("Assignment Operator");
+
+        a = a + 3;
+        System.out.println(a);
+
+        a = a - 3;
+        System.out.println(a);
+
+        
+    }
+}
