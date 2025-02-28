e738dc00c7f9d3e2f4ad78bd95d411dacc26d8f6
Mayur-Shahani <shahanimkwork@gmail.com>
2025-02-28 11:05:45 +0500

    Comparision Operators

---
+++ Comparison_Operators.java
@@ -0,0 +1,26 @@
+package ClassProgram;
+
+public class Comparison_Operators {
+    public static void main(String[] args) {
+         int a = 6;
+         int b = 8;
+
+         System.out.println("Comparision Operators");
+        if(a >= b)
+        {
+            System.out.println("A is Greatest");
+        }
+        else{
+            System.out.println("B is Greatest");
+        }
+
+        if(a <= b)
+        {
+            System.out.println("A is Smallest");
+        }
+        else{
+            System.out.println("B is Smallest");
+        }
+    }
+
+}
