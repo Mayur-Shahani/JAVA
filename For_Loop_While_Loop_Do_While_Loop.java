960b43a1621a456a759e1e1830e06eaa9734922d
Mayur-Shahani <shahanimkwork@gmail.com>
2025-02-28 20:16:52 +0500

    Use of For_While_Do-While Loops

---
+++ For_Loop_While_Loop_Do_While_Loop.java
@@ -0,0 +1,30 @@
+package ClassProgram;
+public class For_Loop_While_Loop_Do_While_Loop {
+    public static void main(String[] args) {
+
+    System.out.println("For Loop");  
+    for(int i = 0; i < 10; i++)
+    {
+        System.out.println(i);
+    }
+
+
+    System.out.println("While Loop");
+    int x = 0;
+    while(x < 10)
+    {
+        System.out.println(x);
+        x++;
+    }
+
+    System.out.println("Do-while Loop");
+    int z = 0;  
+    do
+    {
+        System.out.println(z);
+        z++;
+    }while(z < 10);
+
+    
+ }
+}
