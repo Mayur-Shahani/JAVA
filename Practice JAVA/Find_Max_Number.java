b73d0cc658bca77c52cd827486181283c5827e66
Mayur-Shahani <shahanimkwork@gmail.com>
2025-03-04 22:56:26 +0500

    Find Max Number Using Built-In Function

---
+++ Practice JAVA/Find_Max_Number.java
@@ -0,0 +1,9 @@
+public class Find_Max_Number {
+    public static void main(String[] args) {
+        int a = 11;
+        int b = 7;
+        System.out.println("The Greater Number is "+Math.max(a,b));
+
+    }
+}
+
