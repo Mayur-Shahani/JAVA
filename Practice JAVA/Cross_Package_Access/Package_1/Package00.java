4d20a79588dd2b066c41e49d2709b0222e2b878e
Mayur-Shahani <shahanimkwork@gmail.com>
2025-03-10 10:52:12 +0500

    Added Java package example: Accessing a class from one folder to another

---
+++ Practice JAVA/Cross_Package_Access/Package_1/Package00.java
@@ -0,0 +1,10 @@
+package Package_1;
+
+public class Package00 {
+    private int area = 0;
+    public int area_of_the_rectangle(int length, int width)
+    {
+        area = length * width;
+        return area;
+    }
+}
---
+++ Practice JAVA/Cross_Package_Access/Package_2/Package01.java
@@ -0,0 +1,9 @@
+package Package_2;
+import Package_1.Package00;
+public class Package01 {
+    public static void main(String[] args) {
+        Package00 p1 = new Package00();
+        System.out.println(p1.area_of_the_rectangle(5,5));
+    }
+   
+}
