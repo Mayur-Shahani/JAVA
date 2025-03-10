384489c979e91414f70eb349d08773bab07f8549
Mayur-Shahani <shahanimkwork@gmail.com>
2025-03-10 10:03:50 +0500

    Use of Default Access Modifier

---
+++ Practice JAVA/Use_Of_Default_Modifier/My_Package_1.java
@@ -0,0 +1,10 @@
+package Use_Of_Default_Modifier;
+import Use_Of_Default_Modifier.My_Package_2;
+public class My_Package_1 {
+    public static void main(String[] args) {
+        My_Package_2 a1 = new My_Package_2();
+        System.out.println(a1.area_of_rectangle(4, 5));
+    }
+    
+
+}
