808536818758a17e7ab0313e0af6268083b5f29a
Mayur-Shahani <shahanimkwork@gmail.com>
2025-03-16 16:19:56 +0500

    Default Modifier

---
+++ Use_Of_Default_Modifier/Package_3/Main.java
@@ -0,0 +1,10 @@
+package Package_3;
+import Package_4.Cla;
+
+public class Main {
+    public static void main(String[] args) {
+        Cla c = new Cla();
+       int res = c.square(5);
+       System.out.println(res);
+}
+}
---
+++ Use_Of_Default_Modifier/Package_4/Cla.java
@@ -0,0 +1,9 @@
+package Package_4;
+
+public class Cla {
+    public int square(int x)
+    {
+        return x * x ;
+    }
+    
+}
