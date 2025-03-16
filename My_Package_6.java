dd33a03daf7d8092a5943e3a265d34fcb5040c1e
Mayur-Shahani <shahanimkwork@gmail.com>
2025-03-16 16:24:13 +0500

    Changes Done

---
+++ My_Package_6.java
@@ -0,0 +1,11 @@
+package My_Package;
+
+public class My_Package_2 {
+    
+    private int area = 0;
+    public int area_of_rectangle(int width, int length)
+    {
+        area = width * length;
+        return area;
+    } 
+}
