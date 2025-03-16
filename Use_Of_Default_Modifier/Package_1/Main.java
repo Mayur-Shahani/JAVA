040c3e118fe00a8cb0b5e94adecb27014bee07a1
Mayur-Shahani <shahanimkwork@gmail.com>
2025-03-16 15:29:29 +0500

    Default Modifier

---
+++ Use_Of_Default_Modifier/Package_1/Main.java
@@ -0,0 +1,10 @@
+package Package_1;
+import  Package_2.Cl;
+
+public class Main {
+    public static void main(String[] args) {
+        Cl c = new Cl();
+        c.add(5,4);
+
+    }
+}
---
+++ Use_Of_Default_Modifier/Package_2/Cl.java
@@ -0,0 +1,9 @@
+package Package_2;
+
+public class Cl {
+    public void add(int a, int b)
+    {
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
