073f6c765a787a9f5c320e2e6701d1f77cc1666b
Mayur-Shahani <shahanimkwork@gmail.com>
2025-05-06 21:12:50 +0500

    Added program to find and display the size of an ArrayList in Java.

---
+++ CarListSize.java
@@ -0,0 +1,18 @@
+    import java.util.ArrayList;
+
+    public class CarListSize{
+        public static void main(String[] args) {
+            ArrayList<String> cars = new ArrayList<String>();
+            cars.add("Suzuki");
+            cars.add("Corolla");
+            cars.add("Honda");
+            cars.size();
+    
+            System.out.println(cars.size());
+           
+        }
+        
+    }
+    
+    
+
