a5d2540b2dff58d25d26f564ffacb7973a1d5d17
Mayur-Shahani <shahanimkwork@gmail.com>
2025-05-07 14:27:45 +0500

    Add Java program to demonstrate Stack operations with book titles

---
+++ BookStackDemo.java
@@ -0,0 +1,14 @@
+import java.util.Stack;
+class books{
+    public static void main(String[] args) {
+        Stack <String> books = new Stack <String> ();
+        books.push("History");
+        books.push("Biology");
+        books.push("Chemistry");
+        System.out.println("Top book : " + books.peek());
+
+       
+        
+        
+    }
+}
