3d818fb5ab161fe61ca87a994a4206e8bb1f915f
Mayur-Shahani <shahanimkwork@gmail.com>
2025-05-16 23:53:26 +0500

    Added Java program demonstrating Math functions

---
+++ MathFunctionsDemo.java
@@ -0,0 +1,15 @@
+class Q1{
+    public static void main(String[] args)
+    {
+        System.out.println("Absolute Value : " + Math.abs(-120));
+        System.out.println("Power : " + Math.pow(9,2));
+        System.out.println("Square Root : " + Math.sqrt(81));
+        System.out.println("Maximum Number : " + Math.max(20,-20));
+         System.out.println("Minimum : " + Math.min(34, 0-3));
+        System.out.println("Random Number : " + (int)(Math.random() * 90));
+        System.out.println("Round Of : " + Math.round(56.6));
+        System.out.println("Floor : " + Math.floor(7.86));
+        System.out.println("Round up : " + Math.ceil(4.2));
+
+    }
+}
