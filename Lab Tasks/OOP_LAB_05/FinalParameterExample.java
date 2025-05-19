5c9898cd0208639a9ed57ca1e10f93749aacac99
Mayur-Shahani <shahanimkwork@gmail.com>
2025-05-19 21:01:48 +0500

    Fix error caused by assigning value to a final method parameter in calculateArea()

--- Lab Tasks/OOP_LAB_05/FinalParameterExample.java
+++ Lab Tasks/OOP_LAB_05/FinalParameterExample.java
@@ -1,10 +1,12 @@
  class Circle {
     public static double calculateArea(final double radius) {
         System.out.println(radius);
-        radius = 23.789;
+        radius = 23.789;  //Error: cannot assign a value to final variable 'radius'
         return radius;
     }
+}
 
+    public class FinalParameterExample{
     public static void main(String[] args) {
         System.out.println("Area: " + calculateArea(5.0));
     }
