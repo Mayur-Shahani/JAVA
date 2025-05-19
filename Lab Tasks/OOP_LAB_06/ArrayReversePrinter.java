276a3d2d14e313bb49e46abd81d6766aa752f288
Mayur-Shahani <shahanimkwork@gmail.com>
2025-05-19 21:17:35 +0500

    Add program to print elements of an integer array in reverse order

--- Lab Tasks/OOP_LAB_06/ArrayReversePrinter.java
+++ Lab Tasks/OOP_LAB_06/ArrayReversePrinter.java
@@ -1,6 +1,6 @@
 package OOP_LAB;
 
-public class Task8 {
+public class ArrayReversePrinter {
     public static void main(String[] args) {
         int arr[] = {2,4,5,7,0};
         for(int i = 4; i>=0; i--)
