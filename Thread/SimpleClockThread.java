7991b0683fe996085b7597062cbc0001ab325834
Mayur-Shahani <shahanimkwork@gmail.com>
2025-05-21 12:48:12 +0500

    Implement real-time clock using Java Thread and LocalTime

---
+++ Thread/SimpleClockThread.java
@@ -0,0 +1,30 @@
+package Thread;
+ import java.sql.Time;
+import java.time.LocalTime;
+
+ class SimpleClock extends Thread{
+   public  void run(){
+
+   
+   LocalTime time;
+  
+
+        while(true)
+        {
+            time = LocalTime.now();
+            System.out.println(" Current Time is  : " + time.getHour() + " : " + time.getMinute() + " : " + time.getSecond());
+             try {
+             Thread.sleep(1000);
+        } catch (Exception e) {
+       
+        }
+        }
+ }
+}
+public class SimpleClockThread {
+    public static void main(String[] args) {
+        SimpleClock sc = new SimpleClock();
+        sc.start();
+       
+    }
+}
